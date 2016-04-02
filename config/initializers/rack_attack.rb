de60ac551ab97ee001cd8b736d862c2b62287409
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-04-02 19:34:33 +0200

    Fix minor typos

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,13 +1,12 @@
 class Rack::Attack
   # Throttle all requests by IP (60rpm)
-  # Key: "rack::attack:#{Time.now.to_i/:period}:req/ip:#{req.ip}"
   throttle('req/ip', :limit => 300, :period => 5.minutes) do |req|
-    req.ip # unless req.path.start_with?('/assets')
+    req.ip unless req.path.start_with?('/assets')
   end
 
   spammers = ENV['spammerList'].split(/,\s*/)
 
-  # Turn spammers array into a regexp
+  # Blacklist spammers
   spammer_regexp = Regexp.union(spammers)
   blacklist("block referer spam") do |request|
     request.referer =~ spammer_regexp
