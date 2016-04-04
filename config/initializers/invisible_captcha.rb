79dd2364c9428ebb656180810186d7a288d37abe
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-04-04 21:31:04 +0200

    Remove unwanted config for invisible captcha

--- config/initializers/invisible_captcha.rb
+++ config/initializers/invisible_captcha.rb
@@ -1,8 +1,3 @@
 InvisibleCaptcha.setup do |config|
   config.sentence_for_humans     = 'We do not need you to fill this'
-  config.error_message           = 'You are a robot!'
-  config.honeypots              += 'fake_resource_title'
-  config.visual_honeypots        = false
-  config.timestamp_threshold     = 4.seconds
-  config.timestamp_error_message = 'Sorry, that was too quick! Please resubmit.'
 end
